f3d997160a5844c75f56e776dd0f4cdab5a16873
bmessaoudi <bilelmessaudi@gmail.com>
2023-05-29 15:15:33 +0200

    add construct funtion purple to gradient container

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -5,16 +5,21 @@ var startAlignment = Alignment.topLeft;
 var endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key, required this.colors});
+  const GradientContainer(this.color1, this.color2, {super.key});
 
-  final List<Color> colors;
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
+
+  final Color color1;
+  final Color color2;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: colors,
+          colors: [color1, color2],
           begin: startAlignment,
           end: endAlignment,
         ),
@@ -25,3 +30,25 @@ class GradientContainer extends StatelessWidget {
     );
   }
 }
+
+// class GradientContainer extends StatelessWidget {
+//   const GradientContainer({super.key, required this.colors});
+
+//   final List<Color> colors;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: colors,
+//           begin: startAlignment,
+//           end: endAlignment,
+//         ),
+//       ),
+//       child: const Center(
+//         child: StyledText("Hello!"),
+//       ),
+//     );
+//   }
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.purple,
-        body: GradientContainer(
-          colors: [
-            Color.fromRGBO(103, 58, 183, 1),
-            Color.fromRGBO(49, 20, 100, 1),
-          ],
-        ),
+        body: GradientContainer.purple(),
       ),
     );
   }
